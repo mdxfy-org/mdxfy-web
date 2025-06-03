6094d580e38b55cdb272de5a2cae286ae3efd6fa
Murilo Elias <muriloelias@ipeweb.com.br>
2025-06-03 09:29:17 -0300

    feat: refactor middleware for improved authentication flow and URL handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ export const PUBLIC_PATHS = [
   "/recover-token",
   "/reset-password",
 ];
+
 export const PUBLIC_AUTH_PATHS = ["/auth-code", "/auth-with"];
 
 export const AUTH_TOKEN_KEY = `${process.env.NEXT_PUBLIC_SERVICE_ID}_auth_token`;
@@ -15,71 +16,64 @@ export const AUTH_BROWSER_AGENT_KEY = `${process.env.NEXT_PUBLIC_SERVICE_ID}_aut
 const webHost = new URL(process.env.NEXT_PUBLIC_WEB_BASE_URL as string).host;
 
 const webMiddleware = (request: NextRequest) => {
-  const { pathname } = request.nextUrl;
+  const { pathname, origin } = request.nextUrl;
   const host = request.headers.get("host") || "";
 
-  if (host === webHost) {
-    const hasBrowserAgent = request.cookies.has(AUTH_BROWSER_AGENT_KEY);
-    const hasToken = request.cookies.has(AUTH_TOKEN_KEY);
-    const isAuthenticated = request.cookies.has(AUTHENTICATED_KEY);
-
-    if (PUBLIC_PATHS.includes(pathname)) {
-      if (hasBrowserAgent && hasToken && isAuthenticated) {
-        const redirectUrl = new URL("/", request.url);
-        return NextResponse.redirect(redirectUrl);
-      }
-      return NextResponse.next();
-    }
-
-    if (PUBLIC_AUTH_PATHS.includes(pathname)) {
-      if (!hasBrowserAgent || !hasToken) {
-        const redirectUrl = new URL(PUBLIC_PATHS[0], request.url);
-        return NextResponse.redirect(redirectUrl);
-      }
-      if (isAuthenticated) {
-        const redirectUrl = new URL("/", request.url);
-        return NextResponse.redirect(redirectUrl);
-      }
-      return NextResponse.next();
-    }
-
-    if (!hasBrowserAgent || !hasToken) {
-      const redirectUrl = new URL(PUBLIC_PATHS[0], request.url);
-      return NextResponse.redirect(redirectUrl);
-    }
-    if (!isAuthenticated) {
-      const redirectUrl = new URL(PUBLIC_AUTH_PATHS[0], request.url);
-      return NextResponse.redirect(redirectUrl);
-    }
+  if (host !== webHost) {
+    return NextResponse.next();
   }
+
+  const hasBrowserAgent = request.cookies.has(AUTH_BROWSER_AGENT_KEY);
+  const hasToken = request.cookies.has(AUTH_TOKEN_KEY);
+  const isAuthenticated = request.cookies.has(AUTHENTICATED_KEY);
+
+  if (PUBLIC_PATHS.includes(pathname)) {
+    if (hasBrowserAgent && hasToken && isAuthenticated) {
+      return NextResponse.redirect(new URL("/", origin));
+    }
+    return NextResponse.next();
+  }
+
+  if (PUBLIC_AUTH_PATHS.includes(pathname)) {
+    if (!hasBrowserAgent || !hasToken) {
+      return NextResponse.redirect(new URL(PUBLIC_PATHS[0], origin));
+    }
+    if (isAuthenticated) {
+      return NextResponse.redirect(new URL("/", origin));
+    }
+    return NextResponse.next();
+  }
+
+  if (!hasBrowserAgent || !hasToken) {
+    return NextResponse.redirect(new URL(PUBLIC_PATHS[0], origin));
+  }
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL(PUBLIC_AUTH_PATHS[0], origin));
+  }
+
+  return NextResponse.next();
 };
 
-const publicMatcher = [
-  "/_next/static",
-  "/_next/image",
-  "/img/",
-  "/favicon.ico",
-  "/api",
-  "/_next/data",
-  "/_next",
-  "/static",
-  "/robots.txt",
-  "/sitemap.xml",
-  "/sitemap-index.xml",
-];
-
 export function middleware(request: NextRequest) {
+  const publicMatcher = [
+    "/_next/static",
+    "/_next/image",
+    "/img/",
+    "/favicon.ico",
+    "/api",
+    "/_next/data",
+    "/_next",
+    "/static",
+    "/robots.txt",
+    "/sitemap.xml",
+    "/sitemap-index.xml",
+  ];
+
   if (publicMatcher.some((path) => request.nextUrl.pathname.startsWith(path))) {
     return NextResponse.next();
   }
 
-  const host = request.headers.get("host") || "";
-
-  if (host === webHost) {
-    return webMiddleware(request);
-  }
-
-  return NextResponse.next();
+  return webMiddleware(request);
 }
 
 export const config = {
